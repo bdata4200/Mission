0eea266d901cab41a7b57dc39ef9e5f4338c0807
bdata4200 <167869571+bdata4200@users.noreply.github.com>
2024-04-23 12:57:48 +0200

    Add files via upload

---
+++ setup CDU icons and hasp.bat
@@ -0,0 +1,5 @@
+control date/time
+copy "Shortcut to ContourCDU.exe.lnk" "C:\WINDOWS\DESKTOP"
+copy "Shortcut to ContourCDU.exe.lnk" "c:\WINDOWS\Start Menu\Programs\StartUp"
+copy "CDU2000 Configuration.txt" "C:\Program Files\Contour CDU 2000"
+hdd32
